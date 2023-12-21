88abbbaf21d7c7ce760b87868f869071bd852d93
taorkon <taorkon.tootta@gmail.com>
2023-12-21 17:48:35 +0300

    cleaned js, adapted layout

--- script.js
+++ script.js
@@ -1,67 +1,13 @@
-//Numbers for the game
-let secretNumber = Math.floor(Math.random() * 20) + 1;
-let attempts = 20;
-let record = 0;
-
-//Audios
-const mistakeSound = new Audio("assets/audio/mistake.mp3");
-const loseSound = new Audio("assets/audio/lose.wav");
-const winSound = new Audio("assets/audio/win.mp3");
-const newGame = new Audio("assets/audio/squid-game.mp3");
-
-//Nessesary HTML elements
-//Game stats
-let attemptsHTML = document.querySelector(".attempts");
-let recordHTML = document.querySelector(".record");
-
-//Secret number
-let numberHolder = document.querySelector(".number");
-let numberDisplay = document.querySelector(".number-display");
-
-//Message & input
-let message = document.querySelector(".message");
-let input = document.querySelector("input");
-
-//Buttons
-let checkBtn = document.querySelector(".check");
-let resetBtn = document.querySelector(".reset");
-
-//Check 
-console.log(`secretNumber ${secretNumber}
-attempts ${attempts}
-record ${record}
-attemptsHTML ${attemptsHTML}
-recordHTML ${recordHTML}
-numberHolder ${numberHolder}
-numberDisplay ${numberDisplay}
-message ${message}
-input ${input.value}
-checkBtn ${checkBtn}
-resetBtn ${resetBtn}
-`);
-
-
-//"Reset" button
-resetBtn.addEventListener("click", function() {
+//Reset function
+function reset() {
     secretNumber = Math.floor(Math.random() * 20) + 1;
-    attempts = 20;
+    attempts = 6;
     attemptsHTML.textContent = attempts;
     numberDisplay.textContent = "?";
     message.textContent = `Type a number between 1 & 20...`;
     input.value = "";
     newGame.play();
-})
-
-
-//"Check" button
-checkBtn.addEventListener("click", check);
-
-//Pressin Enter works same as check button
-input.addEventListener('keydown', (value) => {
-    if (value.key === 'Enter') {
-        check();
-    }
-});
+}
 
 //Check function
 function check() {
@@ -78,7 +24,7 @@ function check() {
         message.textContent = `you won!`;
         winSound.play();
 
-        record = (record === 0) || (record > (20 - attempts)) ? (21 - attempts) : record;
+        record = (record === 0) || (record > (6 - attempts)) ? (7 - attempts) : record;
         recordHTML.textContent = record;
 
         //Guess is wrong
@@ -109,3 +55,48 @@ function check() {
         }
     }
 }
+
+//Numbers for the game
+let secretNumber = Math.floor(Math.random() * 20) + 1;
+let attempts = 6;
+let record = 0;
+
+//Audios
+const mistakeSound = new Audio("assets/audio/mistake.mp3");
+const loseSound = new Audio("assets/audio/lose.wav");
+const winSound = new Audio("assets/audio/win.mp3");
+const newGame = new Audio("assets/audio/squid-game.mp3");
+
+//Nessesary HTML elements
+//Game stats
+let attemptsHTML = document.querySelector(".attempts");
+let recordHTML = document.querySelector(".record");
+
+//Secret number
+let numberHolder = document.querySelector(".number");
+let numberDisplay = document.querySelector(".number-display");
+
+//Message & input
+let message = document.querySelector(".message");
+let input = document.querySelector("input");
+
+//Buttons
+let checkBtn = document.querySelector(".check");
+let resetBtn = document.querySelector(".reset");
+let resetMobileBtn = document.querySelector(".reset-mobile");
+
+//Actions for "Reset" buttons (desktop & mobile)
+resetBtn.addEventListener("click", reset)
+resetMobileBtn.addEventListener("click", reset)
+
+//Action for "Check" button
+checkBtn.addEventListener("click", check);
+
+//Action for Enter (same as check button)
+input.addEventListener('keydown', (value) => {
+    if (value.key === 'Enter') {
+        check();
+    }
+});
+
+
